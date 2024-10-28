33a0d74d04e0f315f9f630ce6c0d52f533f32583
Løse Ønline <108939077+loseonline@users.noreply.github.com>
2024-10-28 12:30:50 +0300

    Update userr.js

--- userr.js
+++ userr.js
@@ -3,7 +3,7 @@
 // @version      2.4
 // @namespace    Violentmonkey Scripts
 // @author       mudachyo
-// @match        https://telegram.loseonline.codes/*
+// @match        https://telegram.blum.codes/*
 // @grant        none
 // @icon         https://cdn.prod.website-files.com/65b6a1a4a0e2af577bccce96/65ba99c1616e21b24009b86c_blum-256.png
 // @downloadURL  https://github.com/loseonline/Bl/raw/main/user.js
