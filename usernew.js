e874f272df8afaced4bda657f92654f240bc92c7
Løse Ønline <108939077+loseonline@users.noreply.github.com>
2024-10-28 23:58:46 +0300

    Update usernew.js

--- usernew.js
+++ usernew.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name         LoseOnline Blum 🎃
 // @version      2.0
+// @description Update
 // @namespace    Violentmonkey Scripts
 // @author       LoseOnline
-// @match       https://telegram.blum.codes//*
+// @match       https://telegram.blum.codes/*
 // @grant        none
 // @icon         https://cdn.prod.website-files.com/65b6a1a4a0e2af577bccce96/65ba99c1616e21b24009b86c_blum-256.png
 // @downloadURL  https://github.com/loseonline/Bl/raw/main/usernew.js
